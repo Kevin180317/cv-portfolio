751c38c4236801ae7b0f1c10daa6c1f2c59a8f7a
¨kevin180317¨ <ortega1gv@gmail.com>
2025-07-22 13:16:10 -0700

    integrate sitemap/robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://okhuysen.dev",
+
+  integrations: [sitemap(), robotsTxt()],
+});
