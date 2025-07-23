1223a632151fce56159913911d70fd2973553683
¨kevin180317¨ <ortega1gv@gmail.com>
2025-07-22 18:13:02 -0700

    update logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,14 @@ import sitemap from "@astrojs/sitemap";
 
 import robotsTxt from "astro-robots-txt";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://okhuysen.dev",
-
   integrations: [sitemap(), robotsTxt()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
