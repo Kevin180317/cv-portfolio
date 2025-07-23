3857dd4d15b57f8814d35696a54854670231083e
¨kevin180317¨ <ortega1gv@gmail.com>
2025-07-23 12:06:15 -0700

    canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://okhuysen.dev",
+  site: "https://okhuysen.me",
   integrations: [sitemap(), robotsTxt()],
 
   vite: {
