3b042ff4be08fe33b190f98bc1161e07f9f43a04
¨kevin180317¨ <ortega1gv@gmail.com>
2025-07-24 10:04:56 -0700

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://okhuysen.me",
+  site: "https://okhuysen.me/",
   integrations: [sitemap(), robotsTxt()],
 
   vite: {
